18d7ff567536456b1c1d5cfad5e35bf62c689a79
Victoria Mei <vm930@hotmail.com>
2019-02-27 10:57:45 -0500

    done

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,8 +1,35 @@
 class Restaurant
-  attr_accessor :name
+  @@all = []
+
+  attr_accessor :name,:review
 
   def initialize(name)
     @name = name
+    @@all << self
   end
+
+  def self.all
+    @@all
+  end
+
+  def self.find_by_name(name_str) #i change here from name to name_str so its easier to read 
+    self.all.select do |restaurant|
+      restaurant.name.to_s == name_str
+      end.first 
+  end 
+
+  def customers
+    Review.all.select do |review|
+      review.restaurant = self
+    end.map do |review|
+      review.customers
+    end
+  end 
+
+  def restaurants
+    Review.all.select do |review|
+      review.restaurant = self
+    end
+  end 
 
 end
--- tools/console.rb
+++ tools/console.rb
@@ -7,5 +7,32 @@ end
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
 
+#test Restaurant
+mcdonals = Restaurant.new ("Mcdonals")
+nobu = Restaurant.new ("Nubu")
+italaianmama = Restaurant.new ("Italaian Mama")
+pizzaking = Restaurant.new ("Pizza King")
+chinaexpress = Restaurant.new ("China Express")
+richpeopleonly = Restaurant.new ("Rich Only")
+
+#test Customer
+lebon = Customer.new("Lebon","James")
+bill = Customer.new("Bill","Gates")
+victoria = Customer.new("Victoria","Mei")
+jeff = Customer.new("Jeff","Bezos")
+jayz = Customer.new("jay","Z")
+vicky = Customer.new("Victoria","May")
+
+#test Review
+review1 = Review.new(lebon,chinaexpress)
+review2 = Review.new(lebon,richpeopleonly)
+review3 = Review.new(bill,richpeopleonly)
+review4 = Review.new(bill,italaianmama)
+review5 = Review.new(bill,mcdonals)
+review6 = Review.new(victoria,nobu)
+review7 = Review.new(jayz,nobu)
+review8 = Review.new(jeff,richpeopleonly)
+
+
 binding.pry
 0 #leave this here to ensure binding.pry isn't the last line
