e2906496c0d86671cba7212576c48c3fbd0b17f4
richietam <richardtam.ny@gmail.com>
2019-02-27 10:55:34 -0500

    code challenge 1

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,8 +1,52 @@
 class Restaurant
   attr_accessor :name
 
+  @@all = []
+
   def initialize(name)
     @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.find_by_name(name)
+    self.all.find do |restaurant|
+      restaurant.name == name
+    end
+  end
+
+  def reviews
+    Review.all.select do |review|
+      review.restaurant == self
+    end
+  end
+
+  def customers
+    reviews.map do |review|
+      review.customer
+    end.uniq
+  end
+
+  #this instance method returns avberage star rating based on reviews
+  #use helper method, reviews
+  #use inject method to iterate over rating #
+  #divide sum by reviews.length
+
+  def average_star_rating
+    reviews.inject(0) do |result, review|
+      result + review.rating
+    end / reviews.length
+  end
+
+  #this instance method returns the longest review content for self
+  #sort by content.length
+  def longest_review
+    reviews.sort_by do |review|
+      review.content.length
+    end.last
   end
 
 end
--- tools/console.rb
+++ tools/console.rb
@@ -7,5 +7,25 @@ end
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
 
+#initialize customers
+grumps = Customer.new("Guy", "GrumpyMan")
+fartymcFart = Customer.new("Farty", "McFart")
+hungryHippo = Customer.new("Hungry", "Hippo")
+
+#initialize restaurants
+reallyBad = Restaurant.new("Really Bad Eats")
+reallyGood = Restaurant.new("Really Good Eats")
+
+#Customer adds reviews
+
+review1 = grumps.add_review(reallyBad, "This restaurant was really good but I am so grumpy.", 6)
+review2 = grumps.add_review(reallyGood, "This restaurant was really BAD, I am more grumpy.", 1)
+review3 = grumps.add_review(reallyGood, "TRIED AGAIN. STILL really BAD. WHY ARE THEY CALLED REALLY GOOOOODDD???? I am really grumpy.", -1)
+review4 = fartymcFart.add_review(reallyGood, "This restaurant was really BAD and smelly.", -10000)
+review5 = fartymcFart.add_review(reallyBad, "This restaurant was really GOOD, but really smelly.", 4)
+review6 = hungryHippo.add_review(reallyGood, "GOODFOOD, SMALL PORTION. HUNGRY",5)
+review7 = hungryHippo.add_review(reallyBad, "GOODFOOD, SMALL PORTION. HUNGRY",5)
+
+
 binding.pry
 0 #leave this here to ensure binding.pry isn't the last line
