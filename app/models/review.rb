b642c1fc778adba81d5eb2c6eb462d43f3c7c9c2
Nathaniel Cain <nathaniel2897@gmail.com>
2019-02-27 10:56:32 -0500

    Done.

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,43 @@
 class Review
-  
-end
+  @@all = []
 
+  attr_reader :customer, :restaurant
+  attr_accessor :content, :rating
+
+  def initialize(customer, restaurant, content, rating)
+    @customer = customer
+    @restaurant = restaurant
+    @content = content
+    @rating = rating
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def customer
+    Review.all.select do |review|
+      review.customer == self
+    end
+  end
+
+  def restaurant
+    Review.all.select do |review|
+      review.restaurant == self
+    end
+  end
+
+  def rating
+    Review.all.select do |review|
+      review.rating == self
+    end
+  end
+
+  def content
+    Review.all.select do |review|
+      review.content == self
+    end
+  end
+
+end
--- tools/console.rb
+++ tools/console.rb
@@ -6,6 +6,13 @@ end
 # Insert code here to run before hitting the binding.pry
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
+nathaniel = Customer.new("nathaniel", "cain")
+john = Customer.new("john", "doe")
 
+review1 = Review.new("nathaniel", "arbys", "food was bad", 1)
+review2 = Review.new("john", "ihop", "food was good", 4)
+
+arbys = Restaurant.new("arbys")
+ihop = Restaurant.new("ihop")
 binding.pry
 0 #leave this here to ensure binding.pry isn't the last line
